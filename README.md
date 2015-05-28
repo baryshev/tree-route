b783d6c78ea7c88148dbc463114a8a4e8902ee9d
Vadim Baryshev <vadimbaryshev@gmail.com>
2015-05-28 22:52:26 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,89 @@
+tree-route - request router
+==========================
+
+tree-route is a performance focused request router with regular expressions support.
+
+Instalation
+-----------
+
+Install the latest version with `npm install --save tree-route`
+
+Usage
+-----
+
+Basic usage:
+
+```javascript
+var Router = require('tree-route');
+
+var router = new Router();
+
+// Defining route for several HTTP methods
+router.addRoute(['GET', 'POST'], '/', 'handler0');
+
+// Defining route for one HTTP method
+router.addRoute('GET', '/news', 'handler1');
+
+// Defining route with regular expression param
+router.get('/news/{id:^[0-9]+$}', 'handler2');
+
+// Defining another route with regular expression param
+router.get('/news/{slug:^[a-zA-Z\-]+$}', 'handler3');
+
+// Defining static route that conflicts with previous route, but static routes have high priority
+router.get('/news/all', 'handler4');
+
+// Defining another route
+router.post('/news', 'handler5');
+
+var method = 'GET';
+
+// Optionally pass HEAD requests to GET handlers
+// if (method == 'HEAD') {
+//    method = 'GET';
+// }
+
+var url = '/news/1';
+
+var result = router.dispatch(method, url);
+
+if (typeof result.error === 'undefined') {
+    var handler = result.handler;
+    var params = result.params;
+    // Do something with handler and params
+} else {
+    switch (result.error.code) {
+        case 404 :
+            // Not found handler here
+            break;
+        case 405 :
+            // Method not allowed handler here
+            var allowedMethods = result.allowed;
+            if (method == 'OPTIONS') {
+                // OPTIONS method handler here
+            }
+            break;
+    }
+}
+```
+
+Save and restore routes (useful for routes caching):
+
+```javascript
+var Router = require('tree-route');
+
+var router = new Router();
+
+router.addRoute(['GET', 'POST'], '/', 'handler0');
+router.addRoute('GET', '/news', 'handler1');
+
+var routes = router.getRoutes();
+
+var anotherRouter = new Router();
+var anotherRouter.setRoutes(routes);
+
+var method = 'GET';
+var url = '/news';
+
+var result = anotherRouter.dispatch(method, url);
+```
---
+++ test/router.js
@@ -0,0 +1,71 @@
+var assert = require('assert')
+var Router = require('../lib/router');
+
+var router = new Router();
+describe('Router', function () {
+    router.addRoute(['GET'], '/', 'handler0');
+
+    it('should find existed route', function () {
+        var result = router.dispatch('GET', '/');
+        assert.equal('handler0', result['handler']);
+    });
+
+    it('should return 404 error for not existed route', function () {
+        var result = router.dispatch('GET', '/not/existed/url');
+        assert.equal(404, result['error']['code']);
+    });
+
+    it('should return 405 error for unsupported method', function () {
+        var result = router.dispatch('POST', '/');
+        assert.equal(405, result.error.code);
+        assert.deepEqual(['GET'], result['allowed']);
+    });
+
+    it('should define route with short methods', function () {
+        router.post('/create', 'handler1');
+        var result = router.dispatch('POST', '/create');
+
+        assert.equal('handler1', result['handler']);
+    });
+
+    it('should extract route params', function () {
+        router.get('/news/{id}', 'handler2');
+        var result = router.dispatch('GET', '/news/1');
+
+        assert.equal('handler2', result['handler']);
+        assert.equal('1', result['params']['id']);
+    });
+
+    it('should match regexp in params', function () {
+        router.get('/users/{name:^[a-zA-Z]+$}', 'handler3');
+        router.get('/users/{id:^[0-9]+$}', 'handler4');
+
+        var result = router.dispatch('GET', '/users/@test');
+        assert.equal(404, result['error']['code']);
+
+        var result = router.dispatch('GET', '/users/bob');
+        assert.equal('handler3', result['handler']);
+        assert.equal('bob', result['params']['name']);
+
+        var result = router.dispatch('GET', '/users/123');
+        assert.equal('handler4', result['handler']);
+        assert.equal('123', result['params']['id']);
+    });
+
+    it('should give greater priority to statically defined route', function () {
+        router.get('/users/help', 'handler5');
+        var result = router.dispatch('GET', '/users/help');
+        assert.equal('handler5', result['handler']);
+        assert.deepEqual({}, result['params']);
+    });
+
+    it('should save and restore routes', function () {
+        var routes = router.getRoutes();
+        router = new Router();
+        var result = router.dispatch('GET', '/');
+        assert.equal(404, result['error']['code']);
+        router.setRoutes(routes);
+        var result = router.dispatch('GET', '/');
+        assert.equal('handler0', result['handler']);
+    });
+});
